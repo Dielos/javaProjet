00af5f546917faad475fdee32842e7d55802c66a
Mélody <Mlody@192.168.70.209>
2016-05-09 15:30:38 +0200

    test parser

---
+++ Backend/src/DAO/JpaTypeItemDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.TypeItem;
+
+/**
+ *
+ * @author Robin
+ */
+public class JpaTypeItemDao extends JpaDao<TypeItem> implements TypeItemDao{
+    
+}
---
+++ Backend/test/BDDTests/testDao.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BDDTests;
+
+import DAO.BoxTypeDao;
+import DAO.CommandDao;
+import DAO.DaoFactoryJpa;
+import DAO.JpaBoxTypeDao;
+import DAO.JpaCommandDao;
+import DAO.JpaTypeItemDao;
+import DAO.TypeItemDao;
+import Model.BoxType;
+import Model.Command;
+import Model.TypeItem;
+
+/**
+ *
+ * @author Robin
+ */
+public class testDao {
+    public static void main(String[] args) {
+        BoxTypeDao boxTypeManager = DaoFactoryJpa.getInstance(JpaBoxTypeDao.class);
+        CommandDao commandManager = DaoFactoryJpa.getInstance(JpaCommandDao.class);
+        TypeItemDao typeItemManager = DaoFactoryJpa.getInstance(JpaTypeItemDao.class);
+        
+        BoxType bt1 = new BoxType("b001", 42, 34, 1337);
+        
+        Command c1 = new Command("c001", 42, 33, 98);
+        
+        TypeItem ti1 = new TypeItem("i001", 42, 42, 42, 42, 42);
+                
+        boxTypeManager.create(bt1);
+        
+        commandManager.create(c1);
+        
+        typeItemManager.create(ti1);
+        
+        for (BoxType i : boxTypeManager.findAll()) {
+            System.out.println(i);
+        }
+        
+        for (Command i : commandManager.findAll())
+            System.out.println(i);
+        
+        for (TypeItem i : typeItemManager.findAll())
+            System.out.println(i);
+    }
+}
